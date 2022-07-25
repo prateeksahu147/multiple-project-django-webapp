026b851009c461a7e4004634a0297a641f1345df
PRATEEK SAHU <43596921+prateeksahu147@users.noreply.github.com>
2022-07-25 21:50:02 +0530

    heroku deployment

--- root/wsgi.py
+++ root/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-#from whitenoise.django import DjangoWhiteNoise # for heroku 
+from whitenoise.django import DjangoWhiteNoise # for heroku 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
 
-application = get_wsgi_application()
-#application = DjangoWhiteNoise(application) # for deployment in heroku
+#application = get_wsgi_application()
+application = DjangoWhiteNoise(application) # for deployment in heroku
